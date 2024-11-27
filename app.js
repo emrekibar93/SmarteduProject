2692c106acfec49db7c0014dcac1d8e1468eb710
emrekibar93 <emrekibar93@gmail.com>
2024-11-27 17:32:23 +0300

    Middleware Routes Temlate Engine Added

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const app = express();
 
+//Template Engine
+app.set("view engine", "ejs");
+
+//MIDDLEWARE
+app.use(express.static("public"))
+
+//Routes
 app.get('/',(req,res)=>{
-    res.status(200).send('INDEX SAYFASI');
+    res.status(200).render('index',{page_name: "index"});
+})
+app.get('/about',(req,res)=>{
+    res.status(200).render('about',{page_name: "about"});
 })
 
 
