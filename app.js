74e5298498f11884e736ed5ed2afae34a8bd3669
emrekibar93 <emrekibar93@gmail.com>
2024-11-28 15:15:16 +0300

    Users Course and Category Models added

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const pageController = require("./controllers/pageController");
 const pageRoute = require("./routes/pageRoutes");
+const courseRoute = require("./routes/courseRoute");
+const categoryRoute = require("./routes/categoryRoute");
+const userRoute = require("./routes/userRoute");
 const app = express();
 
 
@@ -9,11 +13,19 @@ app.set("view engine", "ejs");
 
 //MIDDLEWARE
 app.use(express.static("public"))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 //Routes
 app.use('/', pageRoute)
+app.use('/courses',courseRoute)
+app.use('/categories',categoryRoute)
+app.use('/users',userRoute)
 
-
+//connect db
+mongoose.connect("mongodb://localhost/smartedu-db").then(()=>{
+    console.log('Connected DB')
+})
 
 const port = 3000;
 app.listen(port, () => {
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -6,3 +6,11 @@ exports.getIndexPage =(req,res)=>{
 exports.getAboutPage = (req,res)=>{
     res.status(200).render('about',{page_name: "about"});
 }
+
+exports.getCoursePage = (req,res)=>{
+    res.status(200).render('courses',{page_name: "courses"});
+}
+
+exports.getRegisterPage = (req,res)=>{
+    res.status(200).render('register',{page_name: "register"});
+}
